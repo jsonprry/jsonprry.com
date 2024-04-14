82a0277ec4e0d2c948eecdf9a4af861ea9abf04b
Jason <jasonlparry@gmail.com>
2024-04-13 21:56:43 -0600

    testing connection

--- hero.js
+++ hero.js
@@ -1,1 +1,6 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+            var utahElement = document.getElementById("utah");
+            utahElement.addEventListener("click", function() {
+                alert("clicked");
+            });
+        });
